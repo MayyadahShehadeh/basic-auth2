10380e5a682ce905e1de4768702b631d6b706b0b
MayyadahShehadeh <miss.mayadah1@gmail.com>
2023-05-18 12:40:14 +0000

    add basicauth middleware

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const bcrypt = require('bcrypt');
-const base64 = require('base-64');
 const {Users} = require('../models/index');
 const express = require('express');
 const router = express.Router();
-
+const basicAuth = require('../middlewares/basicAuth');
 
 // Signup Route -- create a new user
 // Two ways to test this route with httpie
@@ -29,47 +28,10 @@ router.post('/signup', async (req, res) => {
 // Signin Route -- login with username and password
 // test with httpie
 // http post :3000/signin -a john:foo
-router.post('/signin', async (req, res) => {
+router.post('/signin',basicAuth(Users), async (req, res) => {
 
-    /*
-      req.headers.authorization is : "Basic sdkjdsljd="
-      To get username and password from this, take the following steps:
-        - Turn that string into an array by splitting on ' '
-        - Pop off the last value
-        - Decode that encoded string so it returns to user:pass
-        - Split on ':' to turn it into an array
-        - Pull username and password from that array
-    */
-
-    let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'sdkjdsljd=']
-    console.log('basicHeaderParts',basicHeaderParts);
-
-    let encodedString = basicHeaderParts.pop();  // sdkjdsljd=
-    
-    let decodedString = base64.decode(encodedString); // "username:password"
-    console.log('decodedString',decodedString);
-
-    let [username, password] = decodedString.split(':'); // username, password
-
-    /*
-      Now that we finally have username and password, let's see if it's valid
-      1. Find the user in the database by username
-      2. Compare the plaintext password we now have against the encrypted password in the db
-         - bcrypt does this by re-encrypting the plaintext password and comparing THAT
-      3. Either we're valid or we throw an error
-    */
-    try {
-        const user = await Users.findOne({ where: { username: username } });
-        const valid = await bcrypt.compare(password, user.password);
-        if (valid) {
-            res.status(200).json(user);
-            console.log('valid',valid);
-            
-        }
-        else {
-            throw new Error('Invalid User')
-        }
-    } catch (error) { res.status(403).send("Invalid Login"); }
+    res.status(200).json(req.user);
+   
 
 });
 
